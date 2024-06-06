31eea55a84c7e49ee7f3c08b3d72dcf7c23bfdaa
Aleena <63610006+a-leena@users.noreply.github.com>
2024-06-06 09:47:09 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-import random
-import string
-
-def generate_password(length=12, use_special_chars=True):
-    characters = string.ascii_letters + string.digits
-    if use_special_chars:
-        characters += string.punctuation
-    return ''.join(random.choice(characters) for i in range(length))
